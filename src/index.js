739c4868fac79cca3661ceda21cfb9c7be656540
Gašparovič Boris <gasparovic@BENCONT.local>
2022-02-15 14:23:50 +0100

    simple starter page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
+// import React from "react"; not necesery cause Raact's 17 new JSX transform
 import ReactDOM from "react-dom";
 
-const navbar = (
-  <nav>
-    <h1>Bob's Bistro</h1>
+const page = (
+  <div>
+    <img src="./logo512.png" alt="" width={40} />
+    <h1>Fun facts about React</h1>
     <ul>
-      <li>Menu</li>
-      <li>About</li>
-      <li>Contact</li>
-      <li>test</li>
-      <li>test2</li>
-      <li>test3</li>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars in GitHub</li>
+      <li>It is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
     </ul>
-  </nav>
+  </div>
 );
-ReactDOM.render(navbar, document.getElementById("root"));
+ReactDOM.render(page, document.getElementById("root"));
